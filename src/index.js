83c393dc72746360b5bdf513e7e5a4025a4206d6
FrankFan <fanyong@gmail.com>
2015-12-21 23:26:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -17,16 +17,42 @@ var Promise = require("bluebird");
 var exec = require('child_process').exec;
 var github = require('./githubHelper');
 
-var cmdStr = github.status();
+var cmdStrNewFile = github.newFile();
 
-exec(cmdStr, function(err, stdout, stderr) {
+exec(cmdStrNewFile, function(err, stdout, stderr) {
 		if (err) {
 			console.log('error: ' + stderr);
 		} else {
-			console.log(stdout);
-			
+			// console.log(stdout);
+			var cmdStrAddContent = github.addContent();
+
+			exec(cmdStrNewFile, function(err, stdout, stderr) {
+					if (err) {
+						console.log('error: ' + stderr);
+					} else {
+						// console.log(stdout);
+
+						var cmdStrAdd = github.add();
+						exec(cmdStrAdd, function(err, stdout, stderr) {
+							if (err) {
+								console.log('error: ' + stderr);	
+							} else {
+								var cmdStrCommit = github.commit('update');
+								exec(cmdStrCommit, function(err, stdout, stderr) {
+									if (err) {
+										console.log('error: ' + stderr);	
+									} else {
+										
+									}
+								});
+
+							}
+						});
+					}
+				});
 		}
 	});
+
 
 
 // exec(cmdStr, function(err, stdout, stderr) {
